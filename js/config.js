814f867fd7a4b9f46eea6af5b0f15366b189b589
Amir khan <amierkkhan@gmail.com>
2022-03-09 21:54:28 +0530

    klds

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_5vmLim1hF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7153as7gpvbk1q3pqftp4nfvtu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://tnzxrtfkrj.execute-api.ap-south-1.amazonaws.com/prod/hello' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
